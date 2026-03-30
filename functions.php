424fb831ad7a0dd255b233e5541c0835b3df5dec
Jasper Frumau <jasper@imagewize.com>
2026-03-30 08:21:05 +0700

    Bump version to 2.1.8 in functions.php

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.1.7' );
+define( 'HELLO_ELEMENTOR_CHILD_VERSION', '2.1.8' );
 
 /**
  * Load child theme scripts & styles.
